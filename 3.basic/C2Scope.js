df345de322b971e3638d5cbf7337a4dd2c8623da
monu5092 <monuk1512000@gmail.com>
2024-11-12 10:54:12 +0530

    scope

--- 3.basic/C2Scope.js
+++ 3.basic/C2Scope.js
@@ -1,14 +1,55 @@
-   //
-  
+   //this is global scope
+  let a =300
 
    if(true){
-    let a = 10
-    const b = 20
-    var c = 30
+      // this is block  scope
+      let a = 10
+      const b = 20
+      var c = 30
+     //   console.log(a);
 
    }
 
 
-   console.log(a);
-   console.log(b);
-   console.log(c);
+   //console.log(a);
+   //console.log(b);
+   //console.log(c);
+
+   function one(){
+    const username = "Monu"
+    function two(){
+         const website ="youtube"
+         console.log(username);
+    }
+   // console.log(website);
+      two()
+ }
+
+ //one()
+
+//  if(true){
+//     const username ="Monu"
+//     if(username === "hitesh"){
+//         const website ="youtube"
+//         //console.log(username+website);
+//     }
+//    // console.log(website);  // its give error due to block scope
+//  }
+ //console.log(username);   // ites give error due to block scope
+
+
+
+ //++++++++++++++++++++++++++ Interesting +++++++++++++
+
+
+ console.log(addone(5))
+ function addone(num){
+    return num+1;
+ }
+ 
+
+ addTwo(5)
+ const addTwo = function(num){
+    return num+2
+ }
+ 
