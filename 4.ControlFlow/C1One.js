129147498460782add49059d66c74cc2050ec013
monu5092 <monuk1512000@gmail.com>
2024-11-13 12:12:15 +0530

    Control flow

---
+++ 4.ControlFlow/C1One.js
@@ -0,0 +1,49 @@
+// if 
+
+const isUserloggedIn = true
+const temp = 41
+
+// if(temp === 40){
+//     console.log("less than 55");
+// }else{
+//     console.log("temp is grester than 50");
+// }
+
+// console.log("Execute");
+
+//const score = 200
+
+// if(score>100){
+//     let power ='fly'
+//     console.log(`User power:${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+//const balance = 1000
+
+//if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance>500){
+//     console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance < 900) {
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 12000");
+// }
+
+const isUserLoggedIn =true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+
+if(isUserLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
